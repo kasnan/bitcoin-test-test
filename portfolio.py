352913a9eed0ed3d9e00cc02c806d6c46ed18355
kimsajang <kasnanganji@gmail.com>
2022-11-01 11:11:50 +0900

    수정

--- portfolio.py
+++ portfolio.py
@@ -1,38 +1,36 @@
 from time import time
-from trade.bithumb import bithumb
-# from trade.coinone import sellandbuy
-from trade.korbit import korbit_trade_test
+#from trade.bithumb import bithumb
+from trade.coinone import sellandbuy
+#from trade.korbit import korbit_trade_test
 #from trade.upbit import main
-import pykorbit
+#import pykorbit
 
-_bithumb = bithumb.Bithumb()
-# _coinone = sellandbuy.Coinone()
-_korbit = korbit_trade_test.Korbit()
+#_bithumb = bithumb.Bithumb()
+_coinone = sellandbuy.Coinone()
+#_korbit = korbit_trade_test.Korbit()
 #_upbit = main.Upbit()
 
 
 
-portfolio_btc= _korbit.get_btc_balance() +  +  _bithumb.get_btc_balance() #+ _coinone.get_btc_balance()
+portfolio_btc= _coinone.get_btc_balance() # +  _bithumb.get_btc_balance() +_korbit.get_btc_balance() 
      
-portfolio_cash = _korbit.get_krw_balance()+  _bithumb.get_krw_balance() #+ _coinone.get_krw_balance()        
+portfolio_cash = _coinone.get_krw_balance() #+ _korbit.get_krw_balance()+  _bithumb.get_krw_balance()     
 
 
-portfolio = portfolio_btc * _korbit.get_current_price() + portfolio_cash
-print("profolio : " + str(portfolio))
+#portfolio = portfolio_btc * _coinone.get_current_price() + portfolio_cash
+#print("profolio : " + str(portfolio))
 print("portfolio_btc: " + str(portfolio_btc))
 print("portfolio_krw: " + str(portfolio_cash)+'\n')
 
-averagePrice =_korbit.get_current_price() + _bithumb.get_current_price() #+ _coinone.get_current_price()
-averagePrice = averagePrice/2
+averagePrice =_coinone.get_current_price() #+ _bithumb.get_current_price() #+ _coinone.get_current_price()
+averagePrice = averagePrice/1
 
 print('average  price: ' + str(averagePrice))
 
 
 print('------------------check start time ------------------------')
-print('portfolio: ' + str(portfolio) )
+#print('portfolio: ' + str(portfolio) )
 print('average  price: ' + str(averagePrice))
 #save.insert_row(portFolio, averagePrice, 'any', round_number, trade_center_p)
 
 print('------------------check end time --------------------------')
-
-
